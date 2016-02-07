36fd87441d216d355e60414d851dc480d987986e
David Pham <davidpham87@gmail.com>
2016-02-07 16:24:59 +0100

    docs: Update notations

--- notes/semester_paper/auto/new_semester_paper.el
+++ notes/semester_paper/auto/new_semester_paper.el
@@ -1,28 +1,28 @@
 (TeX-add-style-hook
- "semester_paper_sfs"
+ "new_semester_paper"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("report" "11pt" "a4paper" "twoside" "openright")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("ETHDAsfs" "english") ("natbib" "longnamesfirst") ("xcolor" "dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-environments-local "Linput")
-   (add-to-list 'LaTeX-verbatim-environments-local "Loutput")
-   (add-to-list 'LaTeX-verbatim-environments-local "Sinput")
+                     '(("ETHDAsfs" "english") ("natbib" "longnamesfirst")))
    (add-to-list 'LaTeX-verbatim-environments-local "Soutput")
-   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
-   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
-   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
-   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Sinput")
+   (add-to-list 'LaTeX-verbatim-environments-local "Loutput")
+   (add-to-list 'LaTeX-verbatim-environments-local "Linput")
    (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "Abstract"
@@ -30,6 +30,7 @@
     "theoretical_background"
     "empirical_results"
     "Appendix1"
+    "Appendix2"
     "report"
     "rep11"
     "ETHDAsfs"
@@ -44,19 +45,19 @@
     "relsize"
     "color"
     "fancyvrb"
-    "listings"
-    "xcolor")
+    "listings")
    (TeX-add-symbols
-    '("blue" 1)
-    '("red" 1)
-    '("orange" 1)
-    '("yellow" 1)
-    '("seashell" 1)
-    '("gray" 1)
+    '("HRule" ["argument"] 0)
+    '("file" 1)
     '("Bruch" 2)
     '("aatop" 2)
-    "plim")
+    "plim"
+    "Rcode"
+    "LL"
+    "cmd")
    (LaTeX-add-environments
+    '("Soutput" LaTeX-env-args ["argument"] 0)
+    '("Sinput" LaTeX-env-args ["argument"] 0)
     '("Loutput" LaTeX-env-args ["argument"] 0)
     '("Linput" LaTeX-env-args ["argument"] 0)
     "definition"
